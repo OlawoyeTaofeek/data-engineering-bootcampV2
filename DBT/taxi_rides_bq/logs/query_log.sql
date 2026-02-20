d90d4d691a141491462927c5dde3ff49a05346c1
OlawoyeTaofeek <habephe21@gmail.com>
2026-02-20 13:12:56 +0100

    Analytical Engineering with dbt

--- DBT/taxi_rides_bq/logs/query_log.sql
+++ DBT/taxi_rides_bq/logs/query_log.sql
@@ -1,22 +1,22 @@
--- created_at: 2026-02-20T11:53:25.711115500+00:00
--- finished_at: 2026-02-20T11:53:29.531422500+00:00
--- elapsed: 3.8s
+-- created_at: 2026-02-20T12:05:26.808358900+00:00
+-- finished_at: 2026-02-20T12:05:30.068285900+00:00
+-- elapsed: 3.3s
 -- outcome: success
 -- dialect: bigquery
 -- node_id: not available
--- query_id: TkfYQ7rtWilD7xRma5Mseo10gen
+-- query_id: iJBGjRdJSL0QCeEHiokVzRBuXhw
 -- desc: execute adapter call
 /* {"app": "dbt", "connection_name": "", "dbt_version": "2.0.0", "profile_name": "taxi_rides_bq", "target_name": "dev"} */
 
     select distinct schema_name from `datazoomcamp-486715`.INFORMATION_SCHEMA.SCHEMATA;
   ;
--- created_at: 2026-02-20T11:53:29.580361800+00:00
--- finished_at: 2026-02-20T11:53:33.371625600+00:00
--- elapsed: 3.8s
+-- created_at: 2026-02-20T12:05:30.111686400+00:00
+-- finished_at: 2026-02-20T12:05:33.264390600+00:00
+-- elapsed: 3.2s
 -- outcome: success
 -- dialect: bigquery
--- node_id: model.taxi_rides_bq.assignment2
--- query_id: fAUbPoMVcTSlsfSnDJPcYMd9UjL
+-- node_id: model.taxi_rides_bq.assignment3
+-- query_id: L5eRm9D5BFZdJphfgOccRF1XVcr
 -- desc: get_relation > list_relations call
 SELECT
     table_catalog,
@@ -25,23 +25,21 @@ SELECT
     table_type
 FROM 
     `datazoomcamp-486715`.`ny_taxi_rides`.INFORMATION_SCHEMA.TABLES;
--- created_at: 2026-02-20T11:53:33.379752200+00:00
--- finished_at: 2026-02-20T11:53:36.373933700+00:00
--- elapsed: 3.0s
+-- created_at: 2026-02-20T12:05:33.273383300+00:00
+-- finished_at: 2026-02-20T12:05:35.763521400+00:00
+-- elapsed: 2.5s
 -- outcome: success
 -- dialect: bigquery
--- node_id: model.taxi_rides_bq.assignment2
--- query_id: E0PfVkgiTdRpckswsq0qCUV5D85
+-- node_id: model.taxi_rides_bq.assignment3
+-- query_id: pbVn8FdWmlNobj2hJey2Gi1AEvj
 -- desc: execute adapter call
-/* {"app": "dbt", "dbt_version": "2.0.0", "node_id": "model.taxi_rides_bq.assignment2", "profile_name": "taxi_rides_bq", "target_name": "dev"} */
+/* {"app": "dbt", "dbt_version": "2.0.0", "node_id": "model.taxi_rides_bq.assignment3", "profile_name": "taxi_rides_bq", "target_name": "dev"} */
 
 
-  create or replace view `datazoomcamp-486715`.`ny_taxi_rides`.`assignment2`
+  create or replace view `datazoomcamp-486715`.`ny_taxi_rides`.`assignment3`
   OPTIONS()
   as select
-    sum(total_monthly_trips) as total_trips_oct_2019
-from `datazoomcamp-486715`.`ny_taxi_rides`.`fct_monthly_zone_revenue`
-where service_type = 'Green'
-  and revenue_month BETWEEN DATE '2019-10-01' AND DATE '2019-10-31'   ;
+    count(*) as total_records
+from `datazoomcamp-486715`.`ny_taxi_rides`.`fct_monthly_zone_revenue`;
 
 ;
---
+++ DBT/taxi_rides_bq/target/run/assignment3.sql
@@ -0,0 +1,8 @@
+
+
+  create or replace view `datazoomcamp-486715`.`ny_taxi_rides`.`assignment3`
+  OPTIONS()
+  as select
+    count(*) as total_records
+from `datazoomcamp-486715`.`ny_taxi_rides`.`fct_monthly_zone_revenue`;
+
