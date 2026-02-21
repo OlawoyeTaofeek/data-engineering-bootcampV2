6094cff89cfe6e451b0ff6293c881e4d6a3e5b62
OlawoyeTaofeek <habephe21@gmail.com>
2026-02-21 16:52:34 +0100

    Bigquery Machine Learning

--- Homeworks/03-data-warehouse/bigquery_ml.sql
+++ Homeworks/03-data-warehouse/bigquery_ml.sql
@@ -67,6 +67,23 @@ FROM ML.EXPLAIN_PREDICT(MODEL `datazoomcamp-486715.bq_machine_learning.tip_model
     WHERE tip_amount IS NOT NULL
 ), STRUCT(3 AS top_k_features));
 
+-- HYPER PARAM TUNNING
+CREATE OR REPLACE MODEL `taxi-rides-ny.nytaxi.tip_hyperparam_model`
+OPTIONS
+(model_type='linear_reg',
+input_label_cols=['tip_amount'],
+DATA_SPLIT_METHOD='AUTO_SPLIT',
+num_trials=5,
+max_parallel_trials=2,
+l1_reg=hparam_range(0, 20),
+l2_reg=hparam_candidates([0, 0.1, 1, 10])) AS
+SELECT
+*
+FROM
+`taxi-rides-ny.nytaxi.yellow_tripdata_ml`
+WHERE
+tip_amount IS NOT NULL;
+
 
 
 
